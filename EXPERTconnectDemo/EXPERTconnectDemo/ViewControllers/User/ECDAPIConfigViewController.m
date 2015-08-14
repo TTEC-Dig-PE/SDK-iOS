c69321ee79beb331810fd793cf6ec6dd8f1a1338
kwashington <ken.washington@humanify.com>
2015-08-14 10:24:21 -0600

    Added Rate Response slider to App Config UI

--- EXPERTconnectDemo/EXPERTconnectDemo/ViewControllers/User/ECDAPIConfigViewController.m
+++ EXPERTconnectDemo/EXPERTconnectDemo/ViewControllers/User/ECDAPIConfigViewController.m
@@ -21,13 +21,17 @@ static NSString * const kClearCacheUrlPath = @"/answerengine/v1/clear_cache";
 @property (weak, nonatomic) IBOutlet UITextField *configNameField;
 @property (weak, nonatomic) IBOutlet UITextField *configEndpointField;
 @property (weak, nonatomic) IBOutlet UITextField *configValueField;
+@property (weak, nonatomic) IBOutlet UITextField *sliderValueField;
 @property (weak, nonatomic) IBOutlet UIButton *submitButton;
 @property (weak, nonatomic) IBOutlet UIButton *refreshButton;
 @property (weak, nonatomic) IBOutlet UIButton *askQuestionButton;
+@property (weak, nonatomic) IBOutlet UIButton *submitRatingButton;
 @property (weak, nonatomic) IBOutlet UITextView *answerEngineResponseTextView;
 @property (weak, nonatomic) IBOutlet ECDAdHocAnswerEngineContextPicker *selectAnswerEngineContextPicker;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISlider *rateResponseSlider;
 @property (strong, nonatomic) NSArray *topQuestions;
+@property (strong, nonatomic) ECSAnswerEngineResponse *lastAnswerEngineResponse;
 
 @end
 
@@ -74,6 +78,10 @@ static NSString * const kClearCacheUrlPath = @"/answerengine/v1/clear_cache";
     [btnLayer setMasksToBounds:YES];
     [btnLayer setCornerRadius:5.0f];
     
+    btnLayer = [self.submitRatingButton layer];
+    [btnLayer setMasksToBounds:YES];
+    [btnLayer setCornerRadius:5.0f];
+    
     [self.submitButton addTarget:self
                           action:@selector(submitButtonTapped:)
                 forControlEvents:UIControlEventTouchUpInside];
@@ -83,13 +91,19 @@ static NSString * const kClearCacheUrlPath = @"/answerengine/v1/clear_cache";
                  forControlEvents:UIControlEventTouchUpInside];
     
     [self.askQuestionButton addTarget:self
-                           action:@selector(askQuestionButtonTapped:)
-                 forControlEvents:UIControlEventTouchUpInside];
+                               action:@selector(askQuestionButtonTapped:)
+                     forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.submitRatingButton addTarget:self
+                               action:@selector(submitRatingButtonTapped:)
+                     forControlEvents:UIControlEventTouchUpInside];
 
+    self.submitRatingButton.enabled = false;
     
     [self.selectAnswerEngineContextPicker setup];
 
-
+    self.sliderValueField.text = @"5";
+    
     self.tableView.delegate = self;
     [self refreshButtonTapped:nil];
 }
@@ -175,13 +189,44 @@ static NSString * const kClearCacheUrlPath = @"/answerengine/v1/clear_cache";
     
     ECSURLSessionManager* sessionManager = [[EXPERTconnect shared] urlSession];
     
+    __weak typeof(self) weakSelf = self;
+    
     [sessionManager getAnswerForQuestion:question inContext:context parentNavigator:@"" actionId:@"" questionCount:0
                               customData:nil completion:^(ECSAnswerEngineResponse *response, NSError *error) {
         
         NSLog(@"Received AnswerEngine Response: %@", @"response.answerContent");
                                   
-        self.answerEngineResponseTextView.text = response.answer;
+        weakSelf.answerEngineResponseTextView.text = response.answer;
+        weakSelf.lastAnswerEngineResponse = response;
+        weakSelf.submitRatingButton.enabled = true;
     }];
+}
+
+- (void)submitRatingButtonTapped:(UIButton*)button
+{
+    if(self.lastAnswerEngineResponse == nil) {
+        NSLog(@"No Answer Engine Response to Rate");
+        self.submitRatingButton.enabled = false;
+     
+        return;
+    }
+    
+    NSString *answerid = self.lastAnswerEngineResponse.answerId;
+    NSString *inquiryid = self.lastAnswerEngineResponse.inquiryId;
+    NSNumber *rating = [NSNumber numberWithInt:[self.sliderValueField.text intValue]];
+    
+    ECSURLSessionManager* sessionManager = [[EXPERTconnect shared] urlSession];
+    
+    [sessionManager rateAnswerWithAnswerID:answerid inquiryID:inquiryid parentNavigator:@"" actionId:@""
+                rating:rating questionCount:[NSNumber numberWithInt:0]
+                completion:^(ECSAnswerEngineRateResponse *response, NSError *error) {
+                    
+        NSLog(@"Received AnswerEngine Rate Response:");
+    }];
+}
+
+- (IBAction)sliderValueChanged:(id)sender {
+    self.sliderValueField.text = [NSString stringWithFormat:@"%d", (int)self.rateResponseSlider.value];
 }
 
 - (void) showAlert:(NSString *)title withMessage:(NSString *)message {
