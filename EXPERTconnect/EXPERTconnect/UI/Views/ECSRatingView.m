3226553e73897dc86fcf70cd6b2c32759cadd92b
kwashington <ken.washington@humanify.com>
2015-08-18 09:46:14 -0600

    Checking in ECSRatingView. Apparently missed that on my last commit.

---
+++ EXPERTconnect/EXPERTconnect/UI/Views/ECSRatingView.m
@@ -0,0 +1,14 @@
+//
+//  ECSRatingView.m
+//  EXPERTconnect
+//
+//  Created by Ken Washington on 8/18/15.
+//  Copyright (c) 2015 Humanify, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "ECSRatingView.h"
+
+@implementation ECSRatingView
+@end
