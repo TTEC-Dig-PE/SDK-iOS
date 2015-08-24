ae194896fa3e146f419dcf1c52d11da818a017b9
Sam Solomon <sam.solomon@mutualmobile.com>
2015-08-24 21:15:29 +0530

    Prompt to trigger workflows

--- EXPERTconnectDemo/Horizon/HZPerformanceViewController.m
+++ EXPERTconnectDemo/Horizon/HZPerformanceViewController.m
@@ -27,10 +27,37 @@
 }
 
 - (IBAction)giveFeedbackButtonTapped:(id)sender {
+    
+    UIAlertController *workflowNameController = [UIAlertController alertControllerWithTitle:@"Hello!" message:@"Enter a workflow name" preferredStyle:UIAlertControllerStyleAlert];
+    [workflowNameController addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        [textField setPlaceholder:@"Workflow Name"];
+        [textField setTextAlignment:NSTextAlignmentCenter];
+    }];
+    
+    UIAlertAction *alertActionStop = [UIAlertAction actionWithTitle:@"Back" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+        [workflowNameController dismissViewControllerAnimated:YES completion:nil];
+    }];
+    
+    UIAlertAction *alertActionContinue = [UIAlertAction actionWithTitle:@"Start" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        UITextField *workflowNameTextField = [[workflowNameController textFields] objectAtIndex:0];
+        if(workflowNameTextField.text.length > 0) {
+            [self startWorkflowWith:workflowNameTextField.text];
+        } else {
+            [self presentViewController:workflowNameController animated:YES completion:nil];
+        }
+    }];
+    
+    [workflowNameController addAction:alertActionStop];    
+    [workflowNameController addAction:alertActionContinue];
+    [self presentViewController:workflowNameController animated:YES completion:nil];
+}
+
+-(void)startWorkflowWith:(NSString *)workflowName {
     [[EXPERTconnect shared] setUserIntent:@"mutual funds"];
-    [[EXPERTconnect shared] startWorkflowWithName:@""
+    [[EXPERTconnect shared] startWorkflowWithName:workflowName
                                           delgate:self
                                    viewController:self];
 }
 
+                                          
 @end
