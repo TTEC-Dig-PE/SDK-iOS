2040a929ff5cded1564dfec7b3ed666e61ab41e4
yasararafathmd <yasar.arafath@agiliztech.com>
2015-10-26 19:55:46 +0530

    Workflow section added newly.

---
+++ EXPERTconnectDemo/EXPERTconnectDemo/ViewControllers/User/ECDWorkflowsViewController.h
@@ -0,0 +1,15 @@
+//
+//  ECDWorkflowsViewController.h
+//  EXPERTconnectDemo
+//
+//  Created by AgilizTech Mac on 20/10/15.
+//  Copyright (c) 2015 Humanify, Inc. All rights reserved.
+//
+
+#import "ECDWorkflowsViewController.h"
+#import <EXPERTconnect/ECSRootViewController.h>
+#import <EXPERTconnect/EXPERTconnect.h>
+
+@interface ECDWorkflowsViewController : ECSRootViewController
+
+@end
---
+++ EXPERTconnectDemo/EXPERTconnectDemo/ViewControllers/User/ECDWorkflowsViewController.m
@@ -0,0 +1,27 @@
+//
+//  ECDWorkflowsViewController.m
+//  EXPERTconnectDemo
+//
+//  Created by AgilizTech Mac on 20/10/15.
+//  Copyright (c) 2015 Humanify, Inc. All rights reserved.
+//
+#import "ECDWorkflowsViewController.h"
+
+@interface ECDWorkflowsViewController ()
+
+@end
+
+@implementation ECDWorkflowsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.navigationItem.title = @"Workflow";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
