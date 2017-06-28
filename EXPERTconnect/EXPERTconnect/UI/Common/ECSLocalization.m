5cdf225c030ca2743add0b030237c35722b557e0
Mike Schmoyer <mikeschmoyer@gmail.com>
2017-06-28 15:00:37 -0500

    Adding podspec file for CocoaPod support.

--- EXPERTconnect/EXPERTconnect/UI/Common/ECSLocalization.m
+++ EXPERTconnect/EXPERTconnect/UI/Common/ECSLocalization.m
@@ -6,21 +6,32 @@
 //
 
 #import "ECSLocalization.h"
+#import "EXPERTconnect.h"
 
-NSString* ECSLocalizedString(NSString *key, NSString *comment)
-{
+NSString* ECSLocalizedString(NSString *key, NSString *comment) {
+    
     NSString *string = [[NSBundle mainBundle] localizedStringForKey:key value:nil table:nil];
     
-    if (!string || [string isEqualToString:key])
-    {
+    if (!string || [string isEqualToString:key]) {
+        
         string = [[NSBundle bundleWithIdentifier:@"com.humanify.EXPERTconnect"] localizedStringForKey:key
                                                                                                 value:nil
                                                                                                 table:nil];
     }
-    if( !string )
-    {
-        NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"com.humanify.EXPERTconnect" ofType:@"bundle"];
-        string = [[NSBundle bundleWithPath:bundlePath] localizedStringForKey:key value:nil table:nil];
+    
+    // CocoaPods
+    if( !string || [string isEqualToString:key]) {
+        
+        //NSString *bundlePath = [[NSBundle mainBundle] pathForResource:@"EXPERTconnect" ofType:@"bundle"];
+        NSBundle *ecBundle = [NSBundle bundleForClass:[EXPERTconnect class]];
+        string = [ecBundle localizedStringForKey:key value:nil table:nil];
+    }
+    
+    
+    if( !string ) {
+        
+        NSLog(@"EXPERTconnect: ERROR: SDK localization files not loaded properly.");
+        string = key; // Critical Failure. localizaiton files not working.
     }
     
     return string;
