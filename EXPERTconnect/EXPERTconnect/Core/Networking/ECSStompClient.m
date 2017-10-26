bd88aea075c6ea66200dba00e25dd5feaef9224c
Mike Schmoyer <mikeschmoyer@gmail.com>
2017-10-26 12:51:31 -0500

    PAAS-2437 - Changed ack header to client-individual

--- EXPERTconnect/EXPERTconnect/Core/Networking/ECSStompClient.m
+++ EXPERTconnect/EXPERTconnect/Core/Networking/ECSStompClient.m
@@ -234,11 +234,16 @@ bool        _isConnecting;
     ECSLogDebug(self.logger, @"Subscribing. Dest=%@, SubID=%@", destination, subscriptionID);
     
     NSMutableDictionary *headers = [[NSMutableDictionary alloc] init];
-    headers[@"id"] = subscriptionID;
-    headers[@"destination"] = destination;
-    headers[@"ack"] = @"client";
-    headers[@"persistent"] = @"true";
-    headers[@"prefetch-count"] = @"0"; // mas - Set to 0 as per Russ to avoid a barrage of agent messages out of order.
+    
+    headers[@"id"]                  = subscriptionID;
+    headers[@"destination"]         = destination;
+    
+    // mas - Switched for 6.2.2 per meeting w/Russ,Ken. If ACK sends messageID, this should be -induvidual.
+//    headers[@"ack"] = @"client";
+    headers[@"ack"]                 = @"client-individual";
+    
+    headers[@"persistent"]          = @"true";
+    headers[@"prefetch-count"]      = @"0"; // mas - Set to 0 as per Russ to avoid a barrage of agent messages out of order.
     
     headers[@"receipt"] = kStompReceiptID;
     
